bb8e35c43cfb60bf06ef187aa2596a566eecf7c4
JorjoWasTaken <kiro.nicolov@gmail.com>
2024-04-17 13:42:56 +0200

    Forgot colors

--- bmi-calculator/src/bmi-script.js
+++ bmi-calculator/src/bmi-script.js
@@ -12,21 +12,27 @@ const calculateBmi = () => {
     
     if (bmi < 18.5) {
         bmiGroup.textContent = "Group: body weight deficit";
+        bmiGroup.style.color = "#00a8ff";
     }
     else if (bmi > 18.5 && bmi < 24) {
         bmiGroup.textContent = "Group: norm";
+        bmiGroup.style.color = "#52ff00";
     }
     else if (bmi > 24 && bmi < 30) {
         bmiGroup.textContent = "Group: weight over";
+        bmiGroup.style.color = "#a8ff00";
     }
     else if (bmi > 30 && bmi < 35) {
         bmiGroup.textContent = "Group: obesity first degree";
+        bmiGroup.style.color = "#ffc000";
     }
     else if (bmi > 35 && bmi < 40) {
         bmiGroup.textContent = "Group: obesity second degree";
+        bmiGroup.style.color = "#ff7300";
     }
     else if (bmi > 40) {
         bmiGroup.textContent = "Group: obesity third degree";
+        bmiGroup.style.color = "#ff1d00";
     }
     else {
         console.log("invalid details");
