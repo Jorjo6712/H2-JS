3e970df46bbe6de97034eaea24bcb8c5364f924d
JorjoWasTaken <kiro.nicolov@gmail.com>
2024-04-17 13:06:51 +0200

    Sherlock holmes done

--- Sherlock_Holmes/src/sherlock_holmes.js
+++ Sherlock_Holmes/src/sherlock_holmes.js
@@ -9,6 +9,20 @@ function steal(balance, amount) {
   cameraOn= true;
   }
   var amount = steal(balance, 1250);
-alert("Du er kriminel og du har lige stjålet " +amount +" og det må man ikke!!");
+alert("Du er kriminel og du har lige stjålet " +amount +" og det må man ikke!!!!");
 
-/* Svar: steal funktionen returnerer ikke det nye balance men sit parameter i stedet. udover det, så sætter den en variabel til boolean værdier som tænder og slukker ca meraet, hvilke ikke rigtigt gøre noget fordi cameraet er ikke defineret nogen steder for a styre den via variablens værdi */
+// Svar på Dansk:
+
+/* Svar: steal funktionen returnerer ikke det nye balance men sit parameter i stedet. Det bruger også en global variabel
+som en af sit parameter, nemlig balanace, det er der ikke behov for. 
+Udover det, så sætter den en variabel(cameraOn) til boolean værdier som det meningen at men ville tænde og slukke cameraet med, 
+hvilke ikke rigtigt gøre noget fordi cameraet er ikke defineret nogen steder i koden for at styre den via variablens værdi.
+*/
+
+// Answer in English:
+
+/* Answer: the steal function does not return the new balance but it's parameter instead. It also uses a global variable
+as one of its parameters, namely balanace and there is no need for that.
+In addition to that, it sets a variable (cameraOn) to boolean values that are meant to turn the camera on and off with,
+which don't really do anything because the camera is not defined anywhere in the code to control it via the variable's value.
+*/
